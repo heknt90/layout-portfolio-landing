5a5ee9bceebadba0a4e70d85624027d8f870d7f4
Nikita Efremov <heknt90@mail.ru>
2020-11-04 23:28:50 +0300

    добавлены шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -76,7 +76,7 @@ gulp.task('clean', function () {
 
 gulp.task('copy', function () {
   return gulp.src([
-    'src/fonts/**/*.{woff,woff2}',
+    'src/fonts/**/*.{woff,woff2,ttf}',
     'src/img/**',
     'src/css/**',
     'src/*.html',
